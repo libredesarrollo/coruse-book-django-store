f6846d3e4189487e053809aa5e8606825feca990
andres <desarrollolibre.net@gmail.com>
2025-09-11 12:17:22 -0400

    conf

--- mystore/settings.py
+++ mystore/settings.py
@@ -12,8 +12,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+#PRODUCTION-------
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+#PRODUCTION-------
+
 
 
 # Quick-start development settings - unsuitable for production
