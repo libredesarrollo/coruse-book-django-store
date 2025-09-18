868e8324b01217d05299f6484344be3e0fa2ce6b
andres <desarrollolibre.net@gmail.com>
2025-09-18 08:28:40 -0400

    redireccion

--- mystore/urls.py
+++ mystore/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('elements.urls', namespace='elements')),
+    path('', RedirectView.as_view(url='/store')),  # redirige /store/ → /
 ]
